b2c92319a58839c4f3700d533d1c423a7cba064e
Sultan Raja Marlindo <39815440+Baturaja1337@users.noreply.github.com>
2023-01-30 11:08:23 +0700

    Create get.py

---
+++ get.py
@@ -0,0 +1,14 @@
+import socket
+
+target_host = input("www.example.com" : )
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((target_host, target_port))
+
+request = "GET / HTTP/1.1\r\nHost: " + target_host + "\r\n\r\n"
+client.send(request.encode())
+
+response = client.recv(4096)
+
+print(response.decode())
